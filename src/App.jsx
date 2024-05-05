180e4ee09bd8edf72fd14c603a678f6b8861d906
최정훈 <jeongssi94@gmail.com>
2024-05-05 21:58:43 +0900

    Add: main 베너 css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import './App.css';
 import NavComponent from './commons/NavCompoent';
+import backgroundImage from './images/home-background.png';
 
 function App() {
   return (
-    <div>
+    <MainPage>
       <NavComponent />
-    </div>
+    </MainPage>
   );
 }
 
 export default App;
+
+const MainPage = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw +5px);
+
+  &:after {
+    background: url(${backgroundImage}) center center / cover no-repeat fixed;
+    content: '';
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
